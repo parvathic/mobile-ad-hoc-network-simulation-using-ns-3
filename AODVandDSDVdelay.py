1a303da4f8d6210e4981a02d45399d1cc0273d23
Parvathi Chandrasekhar <parvathichandrasekhar23@gmail.com>
2019-04-12 20:30:07 -0700

    Update AODVandDSDVdelay.py
    removed interactive jupyter notebook lines

--- AODVandDSDVdelay.py
+++ AODVandDSDVdelay.py
@@ -1,21 +1,8 @@
-
-# coding: utf-8
-
-# In[15]:
-
 import numpy as np
 your_file = open('dsdv.txt')
 
-
-# In[16]:
-
-
 flow = your_file.readlines()
 flow
-
-
-# In[17]:
-
 
 #mean delay
 j=0
@@ -38,59 +25,28 @@ for w in range(1, 480, 4):
     q+=1
 print(p3, "\n")
 
-
-# In[18]:
-
-
-# In[19]:
-
-
 b=np.array(p3)
 b2 = [list(map(float, x)) for x in b]
-
-
-# In[20]:
-
 
 a=np.array(m3)
 a2 = [list(map(float, x)) for x in a]
 a2=np.array(a2)
 
-
-# In[21]:
-
-
 c = a2*b2
 c2 = np.multiply(a2, b2)
-
-
-# In[22]:
-
 
 c3 = [list(map(float, x)) for x in c2]
 c3 = np.array(c3)
 
-
-# In[23]:
-
-
 float_formatter = lambda x: "%.2f" % x
 float_formatter(1.6556848e+02)
 np.set_printoptions(formatter={'float_kind':float_formatter})
-
-
-# In[24]:
-
 
 s=0
 i=0
 for i in range(0, 120, 1):
     temp = c3[i]
     s+=temp
-
-
-# In[25]:
-
 
 p3= [list(map(float, x)) for x in p3]
 p4 = np.array(p3)
@@ -100,14 +56,6 @@ for i in range(0, 120, 1):
     tpackets+=temp
 print(tpackets)
 
-
-# In[26]:
-
-
+#final delay
 totaldelay = s/tpackets
-
-
-# In[27]:
-
-
 print(totaldelay)
